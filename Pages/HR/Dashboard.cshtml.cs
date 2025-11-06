4b235129c8418bcc60da3f50f792ec7c0d760a2a
johnchristianjoyo <johnchristianjoyo04@gmail.com>
2025-11-06 17:36:33 +0800

    Create Dashboard.cshtml.cs

---
+++ Pages/HR/Dashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CDM.HRManagement.Pages.HR
+{
+    public class DashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
